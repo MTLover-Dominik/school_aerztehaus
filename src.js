f07a0d77dcafef0b73ce8ef05a671abad2a2c4ec
Dominik Götz <goetzd@bswiesaude.onmicrosoft.com>
2024-10-22 12:11:29 +0200

    added base files

---
+++ src.js
@@ -0,0 +1,8 @@
+﻿button = document.getElementById("doSomething");
+textArea = document.getElementById("doSomethingText");
+
+const message = "Willkommen zu meinem TypeScript-Projekt!";
+
+button.addEventListener("click", ()=> {
+    textArea.innerHTML = message;
+})
