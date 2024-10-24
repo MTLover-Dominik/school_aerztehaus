3fdc8413ef3ba677721b34a11ce312b25e01b5a6
Dominik Götz <goetzd@bswiesaude.onmicrosoft.com>
2024-10-24 09:19:11 +0200

    added login page added base functionality

---
+++ public/functions/login.js
@@ -0,0 +1,14 @@
+﻿
+let loginButton = document.getElementById('login');
+
+loginButton.addEventListener('click', (e) => {
+    handleSubmit(e);
+})
+function handleSubmit(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    // Hier kannst du das machen, was du möchtest, z.B. Daten verarbeiten oder an eine API senden
+    
+}
